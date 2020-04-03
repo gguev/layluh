31ebc115a7f3c84b313a11a4fcb4282c95db804a
ui1092 <56170336+ui1092@users.noreply.github.com>
2020-04-03 16:25:30 -0400

    Added public assets

---
+++ public/js/plyr.js
@@ -0,0 +1,1 @@
+const player=new Plyr("#player",{ratio:"16:9",settings:["speed","loop","quality"],seekTime:5,keyboard:{focused:!0,global:!0},loop:{active:!0},youtube:{modestbranding:"1",iv_load_policy:"3",playsinline:"1",enablejsapi:"1"}});window.player=player;
